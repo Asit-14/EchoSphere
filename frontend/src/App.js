7491f2b2a5a07538381a520840cff880437eff35
asitshakya678 <asitshakya789@gmail.com>
2024-11-12 22:24:07 +0530

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+import { Button } from "@chakra-ui/react"
+// import { Button } from "@/components/ui/button"
+
+const Demo = () => {
+  return <Button>Button</Button>
+}
+
+export default App;
